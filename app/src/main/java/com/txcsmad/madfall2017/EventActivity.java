9a64d3850a8b82942c1f5d4de9122a7762ab1090
root <root@Ali-PC.localdomain>
2017-10-10 19:27:09 -0500

    Finish Lesson 4 Schedule App

--- app/src/main/java/com/txcsmad/madfall2017/EventActivity.java
+++ app/src/main/java/com/txcsmad/madfall2017/EventActivity.java
@@ -1,13 +1,89 @@
 package com.txcsmad.madfall2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class EventActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<String> events;
+    ArrayAdapter<String> adapter;
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        listView = (ListView) findViewById(R.id.event_list);
+        editText = (EditText) findViewById(R.id.edit_text);
+
+        events = new ArrayList<>();
+
+        Intent intent = getIntent();
+        String day = intent.getStringExtra("day");
+
+        switch (day) {
+            case "Monday":
+                events.add("10-11AM Class");
+                events.add("12-2PM Contemplating life");
+                events.add("3-5PM Class");
+                events.add("6-7PM Attempting to do homework");
+                events.add("7-9PM Social Media");
+                events.add("9-12AM Actually doing homework but not really");
+                break;
+            case "Tuesday":
+                // TIK TOK LYRICS lmao
+                events.add("8-9AM Wake up feeling like P-Diddy");
+                events.add("9-11AM Grab glasses and go out the door ready to hit the city");
+                events.add("11AM-12PM Go back to brush teeth");
+                events.add("12PM-12AM Never come back for night");
+                break;
+            case "Wednesday":
+                events.add("10-11AM Class");
+                events.add("12-2PM Contemplating life");
+                events.add("3-5PM Class");
+                events.add("6-7PM Attempting to do homework");
+                events.add("7-9PM Social Media");
+                events.add("9-12AM Actually doing homework but not really");
+                break;
+            case "Thursday":
+                // TIK TOK LYRICS lmao
+                events.add("8-9AM Wake up feeling like P-Diddy");
+                events.add("9-11AM Grab glasses and go out the door ready to hit the city");
+                events.add("11AM-12PM Go back to brush teeth");
+                events.add("12PM-12AM Never come back for night");
+                break;
+            case "Friday":
+                events.add("10-11AM Class");
+                events.add("12-2PM Contemplating life");
+                events.add("3-5PM Class");
+                events.add("6-7PM Attempting to do homework");
+                events.add("7-9PM Social Media");
+                events.add("9-12AM Actually doing homework but not really");
+                break;
+            default:
+                events.add("No events found");
+                break;
+        }
+
+        adapter = new ArrayAdapter<String>(this, R.layout.app_list_item, R.id.text_view, events);
+        listView.setAdapter(adapter);
+    }
+
+    public void updateList(View view) {
+        String update = editText.getText().toString();
+        if(!update.equals(""))
+            events.add(update);
+
+        adapter.notifyDataSetChanged();
+        editText.setText("");
     }
 }
--- app/src/main/java/com/txcsmad/madfall2017/ScheduleActivity.java
+++ app/src/main/java/com/txcsmad/madfall2017/ScheduleActivity.java
@@ -1,7 +1,10 @@
 package com.txcsmad.madfall2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -25,5 +28,16 @@ public class ScheduleActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.schedule_listview);
         adapter = new ArrayAdapter<String>(this, R.layout.app_list_item, R.id.text_view, days);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String element = (String) listView.getItemAtPosition(i);
+
+                Intent intent = new Intent(ScheduleActivity.this, EventActivity.class);
+                intent.putExtra("day", element);
+                startActivity(intent);
+            }
+        });
     }
 }
