f3653faf75db7bcc596da4d2b0d9ae0c6b2e30b3
root <root@Ali.localdomain>
2017-10-19 17:44:45 -0500

    Update Schedule app code

--- app/src/main/java/com/txcsmad/madfall2017/ScheduleActivity.java
+++ app/src/main/java/com/txcsmad/madfall2017/ScheduleActivity.java
@@ -1,31 +1,43 @@
 package com.txcsmad.madfall2017;
 
-import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class ScheduleActivity extends AppCompatActivity {
+    ArrayList<String> days;
+    ArrayAdapter<String> adapter;
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
 
-        Context context = this;
+        days = new ArrayList<String>(Arrays.asList("Monday", "Tuesday",
+                "Wednesday", "Thursday", "Friday"));
 
-        List daysList = Arrays.asList("Sunday", "Monday", "Tuesday",
-                                        "Wednesday", "Thursday", "Friday",
-                                        "Saturday");
+        listView = (ListView) findViewById(R.id.schedule_list);
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.item_text, days);
+        listView.setAdapter(adapter);
 
-        ListView scheduleList = (ListView) findViewById(R.id.schedule_list);
-        ArrayAdapter adapter = new ArrayAdapter(context, R.layout.list_item,
-                                                R.id.item_text, daysList);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String element = (String) listView.getItemAtPosition(i);
 
-        scheduleList.setAdapter(adapter);
+                Intent intent = new Intent(ScheduleActivity.this, EventActivity.class);
+                intent.putExtra("day", element);
+                startActivity(intent);
+            }
+        });
     }
 }
